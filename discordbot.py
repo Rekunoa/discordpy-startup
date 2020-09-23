d09f87afb8e014a0edfd89d78e3c007b567550d8
Rekunoa <71777349+Rekunoa@users.noreply.github.com>
2020-09-24 00:51:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,3 @@
-from discord.ext import commands
-import os
-import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.command()
-async def ねこは(ctx):
-    await ctx.send('かわいい')
-    
-bot.run(token)
 import discord
 from discord.ext import commands
 import asyncio
@@ -64,3 +40,4 @@ class arahabaki_ng_word(commands.Cog):
 
 def setup(bot):
     bot.add_cog(arahabaki_ng_word(bot))
+
